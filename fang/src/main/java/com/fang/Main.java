852f4bc503a95d78e7aac291b3568484fec7d47b
Fang <fangcl@nuaa.edu.cn>
2021-07-02 02:09:13 +0800

    测试

---
+++ fang/src/main/java/com/fang/Main.java
@@ -0,0 +1,11 @@
+package com.fang;
+
+/**
+ * @Author Fang
+ * @create 2021/7/2 2:06
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
